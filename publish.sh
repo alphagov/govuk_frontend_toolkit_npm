09bda843727b2e5fc19560dcc2ef1ccd7e767d79
Gemma Leigh <gemmaleigh@users.noreply.github.com>
2017-02-20 16:36:53 +0000

    Remove Travis-related scripts
    Add comments for additional steps.

--- publish.sh
+++ publish.sh
@@ -27,11 +27,16 @@ rm -f jenkins.sh
 rm -f Gemfile
 rm -f Gemfile.lock
 
+# Remove Travis-related scripts 
+rm push.sh
+rm trigger.sh
+
 # Move the actual toolkit files into the repo where this script is
 rsync -a * ..
 
 cd ..
 
+# Remove govuk_frontend_toolkit directory and tar
 rm -r govuk_frontend_toolkit-master
 rm new-toolkit.tar.gz
 
@@ -41,8 +46,6 @@ VERSION_REGISTRY=`npm view govuk_frontend_toolkit version`
 if [ "$VERSION_LATEST" != "$VERSION_REGISTRY" ]; then
   git add -u
   git add -A
-  git rm push.sh
-  git rm trigger.sh
   git commit -m "Temporary commit: new toolkit files"
   npm version $VERSION_LATEST
   git reset --soft HEAD~2
